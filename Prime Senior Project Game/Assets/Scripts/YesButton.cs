43b59783383bf02d5474637301fac8c5eebee69e
neftalirodriguez <35895973+neftalirodriguez@users.noreply.github.com>
2018-02-23 17:28:37 -0500

    Nef all gui working with bugs

--- Prime Senior Project Game/Assets/Scripts/YesButton.cs
+++ Prime Senior Project Game/Assets/Scripts/YesButton.cs
@@ -11,6 +11,14 @@ public class YesButton : MonoBehaviour {
 		//add points
 		//set scroll possition value to 1(done)
 		//call init()(done)
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
 		Debug.Log("you cliked yes");
 	}
 
