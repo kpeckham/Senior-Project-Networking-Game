8ea437e6e14da62fd1b60d251e99864ced37597b
neftalirodriguez <35895973+neftalirodriguez@users.noreply.github.com>
2018-02-23 17:33:26 -0500

    nef - GUI ALL WORKING WITH BUGS

---
+++ nef/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu2 : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    float sharedVolume;
+
+	public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+        sharedVolume = volume;
+    }
+
+    public void mute()
+    {
+        audioMixer.SetFloat("volume", -80);
+    }
+
+    public void unmute()
+    {
+        audioMixer.SetFloat("volume", sharedVolume);
+    }
+}
---
+++ nef/Assets/Scripts/YesButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YesButton : MonoBehaviour {
+
+	public void clickyes()
+	{
+		//check if yes is correct(done)
+		//add points
+		//set scroll possition value to 1(done)
+		//call init()(done)
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+		Debug.Log("you cliked yes");
+	}
+
+}
