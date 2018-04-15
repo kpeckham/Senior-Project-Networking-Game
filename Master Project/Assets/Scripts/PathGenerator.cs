569675ab35794174d2fff27adbc5b0763342bba5
Andreas Janzen <janzenandy@gmail.com>
2018-04-15 15:02:59 -0400

    Menu changes
    Changes in Menu

--- Master Project/Assets/Scripts/PathGenerator.cs
+++ Master Project/Assets/Scripts/PathGenerator.cs
@@ -11,97 +11,102 @@ using System.Diagnostics;
 //Dijkstra's algorithm borrowed from this website
 //http://www.csharpstar.com/dijkstra-algorithm-csharp/
 
-public class PathGenerator : MonoBehaviour {
-	public List<Text> pathWeights;
-	public int source;
-	public int destination;
-	public int[] distances;
-	// TODO connect nodes and edges to view
-	// TODO add source/destination logic for allowing paths to be tapped
-	// TODO add instruction pop-up
-	// TODO add source/destination information on-screen
-	// TODO figure out scoring
+public class PathGenerator : MonoBehaviour
+{
+    public List<Text> pathWeights;
+    public int source;
+    public int destination;
+    public int[] distances;
+    // TODO connect nodes and edges to view
+    // TODO add source/destination logic for allowing paths to be tapped
+    // TODO add instruction pop-up
+    // TODO add source/destination information on-screen
+    // TODO figure out scoring
 
-	// Use this for initialization
-	void Start () {
-		// TODO Figure out how to call Begin elsewhere and call it with a different level
-		Begin ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // TODO Figure out how to call Begin elsewhere and call it with a different level
+        Begin();
+    }
 
-	void Begin (int level = 1) {
-		int[,] graph = new int[14,14];
+    void Begin(int level = 1)
+    {
+        int[,] graph = new int[14, 14];
 
-		System.Random rand = new System.Random ();
+        System.Random rand = new System.Random();
 
-		List<Tuple<int,int>> edgeList = {{0,1},{1,13},{1,3},{13,2},{3,4},{4,2},
-			{4,5},{6,5},{6,7},{5,9},{9,10},{9,8},{8,12},{11,12},{9,11},{8,7},{13,6}};
-		
-		foreach (Tuple<int,int> edge in edgeList) {
-			int edgeNum = rand.Next (1, level + 5);
-			graph[edge.Item1][edge.Item2] = edgeNum;
-			graph[edge.Item2][edge.Item1] = edgeNum;
-			pathWeights.Add(Convert.ToString(edgeNum));
-		}
+        List<Tuple<int, int>> edgeList = {{0,1},{1,13},{1,3},{13,2},{3,4},{4,2},
+            {4,5},{6,5},{6,7},{5,9},{9,10},{9,8},{8,12},{11,12},{9,11},{8,7},{13,6}};
 
-		List<int> postOfficeOptions = { 0, 2, 7, 10, 12 };
+        foreach (Tuple<int, int> edge in edgeList)
+        {
+            int edgeNum = rand.Next(1, level + 5);
+            graph[edge.Item1][edge.Item2] = edgeNum;
+            graph[edge.Item2][edge.Item1] = edgeNum;
+            pathWeights.Add(Convert.ToString(edgeNum));
+        }
 
-		int sourceIndex = rand.Next (0, 5);
-		source = postOfficeOptions [sourceIndex];
+        List<int> postOfficeOptions = { 0, 2, 7, 10, 12 };
 
-		distances = DijkstraAlgo (graph, source, graph.Length);
+        int sourceIndex = rand.Next(0, 5);
+        source = postOfficeOptions[sourceIndex];
 
-		postOfficeOptions.RemoveAt (sourceIndex);
+        distances = DijkstraAlgo(graph, source, graph.Length);
 
-		int destinationIndex = rand.Next (0, 4);
-		destination = postOfficeOptions [destinationIndex];
-	}
+        postOfficeOptions.RemoveAt(sourceIndex);
 
-	public static int[] DijkstraAlgo(int[,] graph, int source, int verticesCount)
-	{
-		int[] distances = new int[verticesCount];
-		bool[] shortestPathTreeSet = new bool[verticesCount];
+        int destinationIndex = rand.Next(0, 4);
+        destination = postOfficeOptions[destinationIndex];
+    }
 
-		for (int i = 0; i < verticesCount; ++i)
-		{
-			distances[i] = int.MaxValue;
-			shortestPathTreeSet[i] = false;
-		}
+    public static int[] DijkstraAlgo(int[,] graph, int source, int verticesCount)
+    {
+        int[] distances = new int[verticesCount];
+        bool[] shortestPathTreeSet = new bool[verticesCount];
 
-		distances[source] = 0;
+        for (int i = 0; i < verticesCount; ++i)
+        {
+            distances[i] = int.MaxValue;
+            shortestPathTreeSet[i] = false;
+        }
 
-		for (int count = 0; count < verticesCount - 1; ++count)
-		{
-			int u = MinimumDistance(distances, shortestPathTreeSet, verticesCount);
-			shortestPathTreeSet[u] = true;
+        distances[source] = 0;
 
-			for (int v = 0; v < verticesCount; ++v)
-				if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distances[u] != int.MaxValue && distances[u] + graph[u, v] < distances[v])
-					distances[v] = distances[u] + graph[u, v];
-		}
+        for (int count = 0; count < verticesCount - 1; ++count)
+        {
+            int u = MinimumDistance(distances, shortestPathTreeSet, verticesCount);
+            shortestPathTreeSet[u] = true;
 
-		return distances;
-	}
+            for (int v = 0; v < verticesCount; ++v)
+                if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distances[u] != int.MaxValue && distances[u] + graph[u, v] < distances[v])
+                    distances[v] = distances[u] + graph[u, v];
+        }
 
-	private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
-	{
-		int min = int.MaxValue;
-		int minIndex = 0;
+        return distances;
+    }
 
-		for (int v = 0; v < verticesCount; ++v)
-		{
-			if (shortestPathTreeSet[v] == false && distance[v] <= min)
-			{
-				min = distance[v];
-				minIndex = v;
-			}
-		}
+    private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+    {
+        int min = int.MaxValue;
+        int minIndex = 0;
 
-		return minIndex;
-	}
+        for (int v = 0; v < verticesCount; ++v)
+        {
+            if (shortestPathTreeSet[v] == false && distance[v] <= min)
+            {
+                min = distance[v];
+                minIndex = v;
+            }
+        }
 
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        return minIndex;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
