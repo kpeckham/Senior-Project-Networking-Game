1e9bb90e8b3609329e6dc14d8c00ebe0599163b0
Neftali Rodriguez <neftalirodriguez@southern.edu>
2018-04-24 22:36:39 -0400

    most of it is starting to work now.
    levels working
    scaled to android phone and not to computer
    timing set correctly
    fixed level picking
    tieing all of the scenese together

---
+++ Master Project/Assets/Scripts/Checksum Scroller Speeds/CheckSumScroller1.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class CheckSumScroller1 : MonoBehaviour {
+
+    // object we will want to move
+    public GameObject Scroller;
+	public GameObject textBox;
+    public int userpoints;
+    public Text userpoints2;
+
+    private Vector3 startpos;
+    private Vector3 endpos;
+
+    private float distance = 1650f;
+
+    private float time = 10;
+
+    private float currentTime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        startpos = Scroller.transform.position;
+
+        endpos = Scroller.transform.position + Vector3.down * distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		currentTime += Time.deltaTime;
+
+        float perc = currentTime / time;
+
+		if (perc > 1) {
+			currentTime = 0;
+			perc = 0;
+			randomChecksumGenerator randomScript = textBox.GetComponent<randomChecksumGenerator> ();
+			randomScript.Init ();
+
+		}
+		//UnityEngine.Debug.Log (perc);
+        Scroller.transform.position = Vector3.Lerp(startpos, endpos, perc);
+    }
+
+    public void usercorrect()
+    {
+        setTimeOut(true);
+    }
+
+    public void userincorrect()
+    {
+        setTimeOut(false);
+    }
+
+    public void setTimeOut(bool userchoice)
+    {
+        randomChecksumGenerator randomScript = textBox.GetComponent<randomChecksumGenerator> ();
+        if (randomScript.ansCorrect == userchoice)
+        { 
+            Debug.Log("Correct");
+            userpoints +=5;
+            //Debug.Log(userpoints);
+            string number = userpoints.ToString();
+            Debug.Log(number);
+            userpoints2.text = (number);
+        }
+        else {
+            Debug.Log("Incorect");
+            userpoints -=1;
+            //Debug.Log(userpoints);
+            string number = userpoints.ToString();
+            Debug.Log(number);
+            userpoints2.text = (number);
+        }
+        currentTime = 20;
+    }
+	public void resetuserpoints()
+	{
+		userpoints =0;
+        userpoints2.text = " ";
+	}
+
+}
---
+++ Master Project/Assets/Scripts/SegmentationOwn.cs
@@ -0,0 +1,117 @@
+﻿//using System.Collections;
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+
+//public class SegmentationOwn : MonoBehaviour {
+    
+//	// Use this for initialization
+//	void Start () {
+//        int boxLevel = 20;
+//        int numOfBoxes = 0;
+//        int nthNum = 0;
+//        int letterCounter = 0;
+//        int nthLetter = 0;
+//        string letters = "abcd";
+//        List<List<int>> boxes = new List<List<int>>();
+//        List<int> deliBoxes = new List<int>();
+//        bool corBox = false;
+//        List<int> sublist = new List<int>();
+
+//        while (numOfBoxes < boxLevel)
+//        {
+//            int pkgsect;
+//            System.Random RNG = new System.Random();
+//            pkgsect = RNG.Next(1, 4);
+//            int pkgnum;
+//            pkgnum = RNG.Next(1, 25);       // package section (A-D)
+//            pkgnum = pkgnum * 10;           // package number (1-25) multiple of 10
+
+//            int totpkg;
+//            totpkg = pkgsect + pkgnum;
+
+//            deliBoxes.Add(totpkg);
+
+//            numOfBoxes = numOfBoxes + 1;
+//        }
+
+//        int boxSect;
+//        //int boxNum;
+//        for (int i = 0; i < 26; i++)
+//        {
+//            for (int j = 0; j < 4; j++)               // creates three 0 sublists
+//            {
+//                boxSect = 0;
+//                sublist.Add(boxSect);
+//                boxes.Add(sublist);             // add to sublist 
+//            }
+//            //boxNum = 0;
+//            //boxes.Add(boNum)
+           
+
+//            numOfBoxes = numOfBoxes + 1;
+//        }
+
+
+//        for (int k = 0; k<26; k++)
+//        {
+//            if (deliBoxes[k] == boxes.sublist[k] * 10 + 1)
+//            {
+//                corBox = true;
+//            }
+
+//            else
+//            {
+//                corBox = false;
+//            }
+
+//            if (corBox == true)
+//            {
+//                sublist.Add(deliBoxes[k]);
+//                boxes.Add(sublist);                // add box into sublist if the previous box is found in boxes
+//            }
+
+//            else
+//            {
+//                deliBoxes.RemoveAt(k);              // remove box if it the previous box is not a previous box
+//            }
+//        }
+         
+        
+//        //List<int> boxesList = new List<int>();
+//        //boxesList.Add(int1);
+//        //boxesList.Add(int2);
+
+
+//        //List<string> boxesList = new List<string>(new string[] { "%s", "%s", "%s" });
+//    }
+	
+//	// Update is called once per frame
+//	void Update () {
+		
+//	}
+//}
+
+
+
+//letterCounter = 0;
+
+//            if (nthLetter == 7)
+//            {
+//                nthLetter = 0;
+//                nthNum = nthNum + 1;   // integer of the box
+//            }
+//            while (letterCounter == 0)
+//            {
+//                char curLetter = letters[nthLetter]; // letter of the box
+//letterCounter = 1;
+//            }
+//            string boxNumString = nthNum.ToString();   // convert the number of the first box to integer
+
+//string nthBox = boxNumString + "{curLetter}";
+//Console.WriteLine(nthBox);
+//            nthLetter = nthLetter + 1;
+
+//            boxes.Add(nthBox);      // add box just made to string to make a list of boxes 
+
