9e1d25c53f1a0870f07c3b67b62c5a5e7e132fe4
Neftali Rodriguez <neftalirodriguez@southern.edu>
2018-04-30 17:05:41 -0400

    nef 4.30.92

--- Master Project/Assets/Scripts/MainMenu.cs
+++ Master Project/Assets/Scripts/MainMenu.cs
@@ -4,12 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+	public bool CharacterChosen;
+	public bool IsNinaChosen;
+
+
+
+	public void CheckChoosen()
+	{
+		if (PlayerPrefs.HasKey("CharacterIsChosen") && PlayerPrefs.GetInt("CharacterIsChosen") == 1) 
+		{
+			changeMenuScene ("Game Chooser");
+		}
+		else 
+		{
+			changeMenuScene ("Welcome");
+		}
+	}
 
 	public void changeMenuScene(string sceneName)
     {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         SceneManager.LoadScene (sceneName);
     }
+
+	public void CharacterChooseBitFalse()
+	{
+		CharacterChosen = false;
+		PlayerPrefs.SetInt ("CharacterIsChosen", 0);
+	}
+
+	public void CharacterChooseBitTrue()
+	{
+		CharacterChosen = true;
+		PlayerPrefs.SetInt ("CharacterIsChosen", 1);
+	}
+
+	public void NinaChosenfunc()
+	{
+		IsNinaChosen = true;
+		PlayerPrefs.SetInt ("NinaIsChosen", 1);
+	}
+
+	public void WhoIsChosen()
+	{
+		if (IsNinaChosen = true) {
+			Debug.Log ("Nina is Chosen");
+		} else
+			Debug.Log ("Andy is Chosen");
+	}
+		
+
+
 
     public void quitGame()
     {
